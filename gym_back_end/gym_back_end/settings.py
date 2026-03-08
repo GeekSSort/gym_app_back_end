6b1be9a619d5769eed70acf12b211b8d0ef733d3
Shahadat-Hossen-Pabel <shahadathossen01715@gmail.com>
2026-03-08 19:38:35 +0600

    added new

--- gym_back_end/gym_back_end/settings.py
+++ gym_back_end/gym_back_end/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s5gp6lsw-mzturp2^ku5$th^q#f0-^dk7za2a#u^)a)62y@8=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["185.202.223.12", "localhost", "127.0.0.1"]
 
 
 # Application definition
